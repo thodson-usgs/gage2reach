5656a0e633361df35e5a168f874f39a5b177e16e
thodson <thodson@usgs.gov>
2021-05-12 09:45:42 -0500

    Add function to look up NHDPlus flowline

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # gage2reach
 Look up NHDplus v2.1 reach ID for approximately 26,000 U.S. Geological Survey streagages active prior to 2012.
 ```python
-from gage2reach import nhd_reach_id
+from gage2reach import nhd_reach_id, nhd_flowline_id
 reach_id = nhd_reach_id('03339000')
+
+flowline_id = nhd_flowline_id('03339000')
 ```
 NHDplus reach lookup table generated using data at
 https://www.sciencebase.gov/catalog/item/577445bee4b07657d1a991b6
@@ -19,8 +21,8 @@ shape = shapefile.Reader("GageLoc/GageLoc.shp")
 
 shape.records()[0]
 
-reach_lookup_table = dict((row[9], row[2]) for row in shape.records())
+gage_lookup_table = dict((row[9], {'reach_code':row[2], 'comid_flowline':row[-1]}) for row in shape.records())
 
-with bz2.BZ2File("NHDPlus_v2.1_reach_lookup.bz2", 'w') as f:
-    pickle.dump(reach_lookup_table, f)
+with bz2.BZ2File("NHDPlus_v2.1_gage_lookup.bz2", 'w') as f:
+    pickle.dump(gage_lookup_table, f)
 ```
--- gage2reach/__init__.py
+++ gage2reach/__init__.py
@@ -4,18 +4,19 @@ import bz2
 
 package_dir, package_filename = os.path.split(__file__)
 
-LOOKUP_FILE = 'NHDPlus_v2.1_reach_lookup.bz2'
+LOOKUP_FILE = 'NHDPlus_v2.1_gage_lookup.bz2'
 DATA_PATH = os.path.join(package_dir, LOOKUP_FILE)
 
 with bz2.BZ2File(DATA_PATH) as f:
-    reach_lookup_table = pickle.load(f)
+    gage_lookup_table = pickle.load(f)
+
 
 def nhd_reach_id(usgs_gage_id):
     """Look up NHDplus reach ID associated with a USGS streamgage.
-    
+
     Look up NHDplus v2.1 reach ID of approximately 26,000 U.S. Geological Survey
     streamgages active prior to 2012.
-    
+
     Data source:
     https://www.sciencebase.gov/catalog/item/577445bee4b07657d1a991b6
 
@@ -31,11 +32,40 @@ def nhd_reach_id(usgs_gage_id):
     """
     #return(reach_lookup_table[usgs_gage_id])
     try:
-        return(reach_lookup_table[usgs_gage_id])
+        return(gage_lookup_table[usgs_gage_id]['reach_code'])
 
     except KeyError as ke:
         print('No data for streamgage', ke)
         raise
+
+def nhd_flowline_id(usgs_gage_id):
+    """Look up COMID of the NHDPLUS flowline associated with a USGS streamgage.
+
+    Look up NHDplus v2.1 reach ID of approximately 26,000 U.S. Geological Survey
+    streamgages active prior to 2012.
+
+    Data source:
+    https://www.sciencebase.gov/catalog/item/577445bee4b07657d1a991b6
+
+    Parameters
+    ----------
+    usgs_gage_id : str
+        USGS gage ID
+
+    Returns
+    -------
+    int
+        NHDplus flowline COMID
+    """
+    #return(reach_lookup_table[usgs_gage_id])
+    try:
+        return(gage_lookup_table[usgs_gage_id]['comid_flowline'])
+
+    except KeyError as ke:
+        print('No data for streamgage', ke)
+        raise
+
+        
 
 def lookup_table():
     """Return reach ID lookup table.
@@ -45,6 +75,7 @@ def lookup_table():
     dict
     """
     return(reach_lookup_table.copy())
+
 
 def reverse_lookup_table():
     """Return gage ID lookup table.
